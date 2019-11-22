92533bb17ca7b38149ed5ab3f0896a40d3fa6b7a
Karlahuchin <57781382+Karlahuchin@users.noreply.github.com>
2019-11-21 22:50:46 -0600

    Add files via upload

---
+++ Practicas/BDD/com.mayab.calidad.BDD/TestRunner.java
@@ -0,0 +1,13 @@
+package com.mayab.calidad.BDD;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="C://Users//Z4RG4//OneDrive//Documents//PruebasYCalidad_Eclipse//proyectojava//Features//SoundCloud_Play.feature"
+		)
+public class TestRunner {
+
+}
